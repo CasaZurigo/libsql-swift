4c64ea4d525e1d4a1c200d00d3f9bb233c1c81b3
lucaArchidiacono <mail@archiluc.ch>
2026-09-06 23:38:46 +0200

    Ship CLibsql as a dylib xcframework exporting only the libsql C API

--- Sources/CLibsql/build.sh
+++ Sources/CLibsql/build.sh
@@ -4,8 +4,6 @@ set -xe +f
 
 cd libsql-c
 
-export IPHONEOS_DEPLOYMENT_TARGET=15.1
-
 function build_ios() {
     iphone=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
     iphonesimulator=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
