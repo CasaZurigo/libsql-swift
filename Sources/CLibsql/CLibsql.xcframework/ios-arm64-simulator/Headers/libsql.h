3f475e3f6d4727cce310a087046b8b2d34aaf7c9
lucaArchidiacono <mail@archiluc.ch>
2026-09-06 11:47:47 +0200

    Rebuild CLibsql dylib with disable_remote_writes support

--- Sources/CLibsql/CLibsql.xcframework/ios-arm64-simulator/Headers/libsql.h
+++ Sources/CLibsql/CLibsql.xcframework/ios-arm64-simulator/Headers/libsql.h
@@ -138,6 +138,10 @@ typedef struct {
     bool synced;
     /** Safety assert */
     bool disable_safety_assert;
+    /** Provide a namespace through a header. */
+    const char *namespace;
+    /** If set, writes of synced databases are applied locally and pushed to the remote by the sync interval, instead of being sent to the remote primary. */
+    bool disable_remote_writes;
 } libsql_database_desc_t;
 
 typedef struct {
